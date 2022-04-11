7f684c920f2b41400049d7a1711995a0bb63cc10
MrAhmalo <ahmalo456@gmail.com>
2022-04-11 17:22:42 +0200

    Filled und Empty Electronic Bottle hinzugefügt

---
+++ src/main/java/net/mcreator/energized/item/ElectronicBottleItem.java
@@ -0,0 +1,73 @@
+
+package net.mcreator.energized.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.energized.procedures.ElectronicBottleItemIsDroppedByPlayerProcedure;
+import net.mcreator.energized.procedures.ElectronicBottleFoodEatenProcedure;
+import net.mcreator.energized.init.EnergizedModTabs;
+import net.mcreator.energized.init.EnergizedModItems;
+
+import java.util.List;
+
+public class ElectronicBottleItem extends Item {
+	public ElectronicBottleItem() {
+		super(new Item.Properties().tab(EnergizedModTabs.TAB_ENERGIZED).stacksTo(64).rarity(Rarity.RARE)
+				.food((new FoodProperties.Builder()).nutrition(4).saturationMod(0.3f).alwaysEat()
+
+						.build()));
+		setRegistryName("electronic_bottle");
+	}
+
+	@Override
+	public int getUseDuration(ItemStack stack) {
+		return 40;
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.DRINK;
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+		list.add(new TextComponent("Gives you an electric shock"));
+	}
+
+	@Override
+	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
+		ItemStack retval = new ItemStack(EnergizedModItems.EMPTY_ELECTRONIC_BOTTLE);
+		super.finishUsingItem(itemstack, world, entity);
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+
+		ElectronicBottleFoodEatenProcedure.execute(world, x, y, z, entity);
+		if (itemstack.isEmpty()) {
+			return retval;
+		} else {
+			if (entity instanceof Player player && !player.getAbilities().instabuild) {
+				if (!player.getInventory().add(retval))
+					player.drop(retval, false);
+			}
+			return itemstack;
+		}
+	}
+
+	@Override
+	public boolean onDroppedByPlayer(ItemStack itemstack, Player entity) {
+		ElectronicBottleItemIsDroppedByPlayerProcedure.execute(entity.level, entity.getX(), entity.getY(), entity.getZ());
+		return true;
+	}
+}
