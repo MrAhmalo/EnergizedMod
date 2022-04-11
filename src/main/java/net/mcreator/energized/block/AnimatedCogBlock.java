76fe02f0466cb4be4a78a380640228890a26d236
MrAhmalo <ahmalo456@gmail.com>
2022-04-11 16:49:26 +0200

    CreativeTab logo geändert

--- src/main/java/net/mcreator/energized/block/AnimatedCogBlock.java
+++ src/main/java/net/mcreator/energized/block/AnimatedCogBlock.java
@@ -1,16 +1,30 @@
 
 package net.mcreator.energized.block;
 
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.BlockPos;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+
+import net.mcreator.energized.init.EnergizedModBlocks;
+
+import java.util.List;
+import java.util.Collections;
 
 public class AnimatedCogBlock extends Block {
-
 	public AnimatedCogBlock() {
 		super(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.GRAVEL).strength(1f, 10f).noOcclusion()
 				.isRedstoneConductor((bs, br, bp) -> false));
-
 		setRegistryName("animated_cog");
 	}
 
@@ -26,7 +40,6 @@ public class AnimatedCogBlock extends Block {
 
 	@Override
 	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
--- src/main/java/net/mcreator/energized/init/EnergizedModTabs.java
+++ src/main/java/net/mcreator/energized/init/EnergizedModTabs.java
@@ -7,7 +7,6 @@ package net.mcreator.energized.init;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.CreativeModeTab;
 
@@ -18,7 +17,7 @@ public class EnergizedModTabs {
 		TAB_ENERGIZED = new CreativeModeTab("tabenergized") {
 			@Override
 			public ItemStack makeIcon() {
-				return new ItemStack(Items.BLAZE_ROD);
+				return new ItemStack(EnergizedModBlocks.ANIMATED_COG);
 			}
 
 			@OnlyIn(Dist.CLIENT)
