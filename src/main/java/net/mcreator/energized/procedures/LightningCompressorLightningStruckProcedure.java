d2c70f5aff4a9e6be497824ed7114baf30437040
MrAhmalo <draco03draco@gmail.com>
2022-04-12 13:27:08 +0200

    LightningCommpressor Funktion erstellt

---
+++ src/main/java/net/mcreator/energized/procedures/LightningCompressorLightningStruckProcedure.java
@@ -0,0 +1,49 @@
+package net.mcreator.energized.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.energized.init.EnergizedModFluids;
+import net.mcreator.energized.init.EnergizedModBlocks;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class LightningCompressorLightningStruckProcedure {
+	@SubscribeEvent
+	public static void onEntitySpawned(EntityJoinWorldEvent event) {
+		Entity entity = event.getEntity();
+		execute(event, event.getWorld(), entity.getX(), entity.getY(), entity.getZ(), entity);
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof LightningBolt) {
+			if ((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == EnergizedModBlocks.LIGHTNING_COMPRESSOR) {
+				{
+					BlockEntity _ent = world.getBlockEntity(new BlockPos((int) x, (int) y, (int) z));
+					int _amount = 1000;
+					if (_ent != null)
+						_ent.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, null).ifPresent(capability -> capability
+								.fill(new FluidStack(EnergizedModFluids.COMPRESSED_LIGHTNING, _amount), IFluidHandler.FluidAction.EXECUTE));
+				}
+			}
+		}
+	}
+}
